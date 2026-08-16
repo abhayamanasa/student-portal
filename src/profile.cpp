6379271c9a6b9b52e2c9157e583624b295bd0f90
abhayamanasa <dasariabhayamanasa@gmail.com>
2026-08-16 13:19:31 +0530

    Add reusable student name accessor

--- src/profile.cpp
+++ src/profile.cpp
@@ -3,9 +3,14 @@
 
 using namespace std;
 
+string getStudentName()
+{
+    return "Abhaya Manasa";
+}
+
 void displayProfileInformation()
 {
-    cout << "Name       : Abhaya Manasa\n";
+    cout << "Name       : " << getStudentName() << "\n";
     cout << "Student ID : 2026202014\n";
     cout << "Department : Computer Science\n";
     cout << "Year       : I Year\n";
--- src/profile.h
+++ src/profile.h
@@ -1,6 +1,10 @@
 #ifndef PROFILE_H
 #define PROFILE_H
 
+#include <string>
+
+std::string getStudentName();
+
 void displayProfileInformation();
 void showProfile();
 
