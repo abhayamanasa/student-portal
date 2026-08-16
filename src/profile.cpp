ad8d778ac75006fa8898baf2743b2b54c1e63d4d
abhayamanasa <dasariabhayamanasa@gmail.com>
2026-08-16 11:42:16 +0530

    Add student profile page

---
+++ src/profile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "profile.h"
+
+using namespace std;
+
+void showProfile()
+{
+    cout << "\n========== STUDENT PROFILE ==========\n";
+
+    cout << "Name       : Abhaya Manasa\n";
+    cout << "Student ID : 2026202014\n";
+    cout << "Department : Computer Science\n";
+    cout << "Year       : I Year\n";
+    cout << "Email      : student@iiit.com\n";
+
+    cout << "=====================================\n";
+}
