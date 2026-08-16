05e7e6b6fc3729aefdedbfd94885bbfe732ffa25
abhayamanasa <dasariabhayamanasa@gmail.com>
2026-08-16 15:38:18 +0530

    Add academic year accessor

--- src/profile.h
+++ src/profile.h
@@ -7,6 +7,7 @@ std::string getStudentName();
 std::string getStudentId();
 std::string getDepartment();
 std::string getEmail();
+std::string getAcademicYear();
 
 void displayProfileInformation();
 void showProfile();
