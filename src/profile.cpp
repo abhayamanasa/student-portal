5ec347478e0c962c152510fc2a5cc01bd5df5fcc
abhayamanasa <dasariabhayamanasa@gmail.com>
2026-08-16 13:18:24 +0530

    Separate profile information display

--- src/profile.cpp
+++ src/profile.cpp
@@ -3,15 +3,20 @@
 
 using namespace std;
 
-void showProfile()
+void displayProfileInformation()
 {
-    cout << "\n========== STUDENT PROFILE ==========\n";
-
     cout << "Name       : Abhaya Manasa\n";
     cout << "Student ID : 2026202014\n";
     cout << "Department : Computer Science\n";
     cout << "Year       : I Year\n";
     cout << "Email      : student@iiit.com\n";
+}
+
+void showProfile()
+{
+    cout << "\n========== STUDENT PROFILE ==========\n";
+
+    displayProfileInformation();
 
     cout << "=====================================\n";
 }
